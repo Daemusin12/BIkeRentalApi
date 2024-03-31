6e048bc66ef54fe0bb27a2419ddb52481a071714
MSI\Leo <engrleonelfelezario812@gmail.com>
2024-03-31 11:40:14 +0800

    Add User and UserDto Models

---
+++ Models/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BIkeRentalApi.Models
+{
+    public class UserDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+    }
+}
